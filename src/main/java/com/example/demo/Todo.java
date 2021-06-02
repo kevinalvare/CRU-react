74fd43c85724ed613879fcb1fd40b9765265f931
KEVIN ALVAREZ <keymar1891@gmail.com>
2021-06-01 20:12:11 -0500

    Crear proyecto con Spring boot

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import javax.persistence.Entity;
+
+@Entity
+
+public class Todo {
+    private  long id;
+    private  String name;
+    private boolean isCompleted;
+
+}
